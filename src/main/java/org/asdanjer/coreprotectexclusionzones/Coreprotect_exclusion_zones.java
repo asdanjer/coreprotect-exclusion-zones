3767751f30a8aa691dd6f9206c01614a768786b5
asdanjer <17290236+asdanjer@users.noreply.github.com>
2024-08-31 21:23:12 +0200

    inital

--- src/main/java/org/asdanjer/coreprotectexclusionzones/Coreprotect_exclusion_zones.java
+++ src/main/java/org/asdanjer/coreprotectexclusionzones/Coreprotect_exclusion_zones.java
@@ -1,14 +1,32 @@
 package org.asdanjer.coreprotectexclusionzones;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import net.coreprotect.CoreProtect;
+import net.coreprotect.CoreProtectAPI;
 
 public final class Coreprotect_exclusion_zones extends JavaPlugin {
+    private CoreProtectAPI getCoreProtect() {
+        Plugin plugin = getServer().getPluginManager().getPlugin("CoreProtect");
 
+        // Check that CoreProtect is loaded
+        if (plugin == null || !(plugin instanceof CoreProtect)) {
+            return null;
+        }
+    }
     @Override
     public void onEnable() {
         // Plugin startup logic
+        CoreProtectAPI coreProtect = getCoreProtect();
+        if (coreProtect == null) {
+            getLogger().info("CoreProtect not found, disabling plugin.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        getLogger().info("CoreProtect found, enabling plugin.");
 
     }
+
 
     @Override
     public void onDisable() {
