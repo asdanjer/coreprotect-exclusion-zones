ced6fe5a710d109da3ae4e633da77114ae9ae010
asdanjer <17290236+asdanjer@users.noreply.github.com>
2024-08-31 21:02:54 +0200

    inital

---
+++ src/main/java/org/asdanjer/coreprotectexclusionzones/Coreprotect_exclusion_zones.java
@@ -0,0 +1,17 @@
+package org.asdanjer.coreprotectexclusionzones;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Coreprotect_exclusion_zones extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
